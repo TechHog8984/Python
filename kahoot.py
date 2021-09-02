c46c92ae5c25933d11f3b36a5017be2329728bb0
TechHubOP <77560013+TechHubOP@users.noreply.github.com>
2021-09-02 18:44:55 -0400

    Update kahoot.py

--- kahoot.py
+++ kahoot.py
@@ -1,4 +1,5 @@
 import requests, random
+from threading import Thread
 
 def check(pin):
     req = requests.get(f'https://kahoot.it/reserve/session/{pin}')
@@ -6,6 +7,11 @@ def check(pin):
         return req.json()
     except:
         return False
+def gen():
+            while True:
+                pin = random.randint(1000000, 9999999)
+                if check(pin):
+                    print(f'{pin}: success')
 
 def main():
     t = input('Check a pin(1) or gen pins(2): ')
@@ -16,10 +22,22 @@ def main():
         else:
             print(f'{pin}: fail')
     elif '2' in t:
-        while True:
-            pin = random.randint(1000000, 9999999)
-            if check(pin):
-                print(f'{pin}: success')
+        dothreading = input('Use threading?(y/n): ') == 'y'
+        if dothreading:
+            threads = []
+            try:
+                threadamount = int(input('Thread amount: '))
+            except:
+                threadamount = 5
+            for i in range(threadamount + 1):
+                thread = Thread(target=gen)
+                threads.append(thread)
+            for i in threads:
+                i.start()
+            for i in threads:
+                i.join()
+        else:
+            gen()
     main()
 
 if __name__ == '__main__':

b823f10aa337aedfa511ac27293d7c446e6ad34e
TechHubOP <77560013+TechHubOP@users.noreply.github.com>
2021-09-11 17:21:45 -0400

    Add files via upload

---
+++ bonk.py
@@ -0,0 +1,55 @@
+import os, requests, time, string, random
+
+url = 'https://bonk2.io/scripts/register_legacy.php'
+proxies_file = open('proxies.txt')
+proxies_text = proxies_file.read()
+proxies_file.close()
+proxies = []
+for line in proxies_text.split('\n'):
+    if line and len(line) > 2:
+        proxies.append(line)
+
+def unpack(s):
+    result = ''
+    for a in s:
+        result += a
+    return result
+def getpassword():
+    return unpack(random.choices(string.ascii_letters + '!@#$%^&*(){}:".,<>', k = 10))
+
+def getproxy():
+    r = random.randint(0, len(proxies) - 1)
+    return f'http://{proxies[r]}'
+
+def send(name):
+    print(f'attempting to create {name}')
+    try:
+        p = getpassword()
+        req = requests.post(url = url, data = {'username' : name, 'password' : p, 'remember' : False})
+        try:
+            success = req.json().get('r')
+            if success == 'success':
+                file = open('bonk results.txt', 'a')
+                file.write(f'{name} : {p}\n')
+                file.close()
+                return f'{success}\n{name} : {p}'
+            else:
+                return req.json()
+        except:
+            pass
+    except:
+        pass
+    return f'failed to create {name}'
+
+if __name__ == '__main__':
+    clear = lambda: os.system('clear')
+    clear()
+    input('you are going to get rate limited after a while, beware ')
+    basename = 'tZ1choSgF'
+    name = f'{basename}1'
+    n = 1
+    while True:
+        print(send(name))
+        n += 1
+        name = f'{basename}{n}'
+        time.sleep(2)
